4f3bdc3bf9a7bac52a65827919d671dda1d44e6e
wnsgh7368 <sin010820@gmail.com>
2026-04-07 14:27:33 +0900

    chore: swagger 설정 및 도메인형 아케틱쳐 세팅

---
+++ oasis/week4/umc10th/src/main/java/com/example/umc10th/domain/inquiry/exception/InquriryException.java
@@ -0,0 +1,7 @@
+package com.example.umc10th.domain.inquiry.exception;
+
+public class InquriryException extends RuntimeException {
+    public InquriryException(String message) {
+        super(message);
+    }
+}
