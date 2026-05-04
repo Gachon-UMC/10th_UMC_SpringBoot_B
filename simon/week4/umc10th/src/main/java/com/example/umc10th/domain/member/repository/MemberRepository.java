58798d5859b6147c618bd5176775fdb026add7f9
Donghoon Seo <naosdh@naver.com>
2026-05-05 03:11:49 +0900

    [FEAT] 마이페이지 조회 쿼리

--- simon/week4/umc10th/src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
+++ simon/week4/umc10th/src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
@@ -1,9 +1,23 @@
 package com.example.umc10th.domain.member.repository;
 
+import com.example.umc10th.domain.member.dto.MemberResDTO;
 import com.example.umc10th.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("""
+            select new com.example.umc10th.domain.member.dto.MemberResDTO$GetInfo(
+                m.name,
+                m.profileUrl,
+                m.email,
+                m.phoneNumber,
+                m.point
+            )
+            from Member m
+            where m.id = :memberId
+            """)
+    Optional<MemberResDTO.GetInfo> findGetInfoById(Long memberId);
 }
